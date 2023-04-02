8e9897782a9a03b6e96f0cd8f2dd438b2be816d1
Karl Hiner <karl.hiner@gmail.com>
2023-04-02 01:44:49 -0400

    Remove const qualifiers in `fragment.glsl`. It's apparently not a thing, and wasn't erroring because it wasn't replacing these res files.

--- app/res/shaders/fragment.glsl
+++ app/res/shaders/fragment.glsl
@@ -21,29 +21,29 @@ uniform vec4 ambient, diffuse, specular;
 uniform float shininess;
 
 vec4 compute_lighting(vec3 direction, vec4 lightcolor, vec3 normal, vec3 halfvec, vec4 mydiffuse, vec4 myspecular, float myshininess) {
-    const float n_dot_l = dot(normal, direction);
-    const vec4 lambert = mydiffuse * lightcolor * max (n_dot_l, 0.0);
-    const vec4 phong = myspecular * lightcolor * pow (max(dot(normal, halfvec), 0.0), myshininess);
+    float n_dot_l = dot(normal, direction);
+    vec4 lambert = mydiffuse * lightcolor * max(n_dot_l, 0.0);
+    vec4 phong = myspecular * lightcolor * pow(max(dot(normal, halfvec), 0.0), myshininess);
     return lambert + phong;
 }       
 
 void main (void) {
     // Calculate camera direction
-    const vec3 eyepos = vec3(0,0,0);
-    const vec3 mypos = myvertex.xyz / myvertex.w;
-    const vec3 eyedirn = normalize(eyepos - mypos);
+    vec3 eyepos = vec3(0,0,0);
+    vec3 mypos = myvertex.xyz / myvertex.w;
+    vec3 eyedirn = normalize(eyepos - mypos);
 
     // Normalise the normal at that point.
-    const vec3 normal = normalize(mynormal);
+    vec3 normal = normalize(mynormal);
 
     // Start with ambient color, and iterate through each light source (on or off).
     vec4 finalcolor = vec4(ambient);
     for (int i = 0; i < num_lights; i++) {
-        const vec4 light_position = inverse(modelview) * light_posn[i];
-        const vec3 position = light_position.xyz / light_position.w;
-        const vec3 direction = normalize(position - mypos);
-        const vec3 half_i = normalize(direction + eyedirn);
-        const vec4 light_color = light_col[i];
+        vec4 light_position = inverse(modelview) * light_posn[i];
+        vec3 position = light_position.xyz / light_position.w;
+        vec3 direction = normalize(position - mypos);
+        vec3 half_i = normalize(direction + eyedirn);
+        vec4 light_color = light_col[i];
 
         // Compute light based on the normal at the point and the light direction.
         finalcolor += compute_lighting(direction, light_color, normal, half_i, diffuse, specular, shininess);
