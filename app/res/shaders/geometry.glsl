8f09e742a3fd213d22fd3c0180ac0cbcc61d177b
Karl Hiner <karl.hiner@gmail.com>
2023-09-25 05:57:53 -0400

    DRY up geometry shader.

--- app/res/shaders/geometry.glsl
+++ app/res/shaders/geometry.glsl
@@ -2,7 +2,7 @@
 
 // Inputs from vertex shader
 layout (triangles) in;
-layout (triangle_strip, max_vertices = 18) out; // Max 6 vertices per quad, 3 edges -> 18 vertices
+layout (triangle_strip, max_vertices = 12) out;
 
 uniform float line_width;
 uniform int draw_lines;
@@ -15,61 +15,37 @@ out vec3 geom_vertex_normal;
 out vec4 geom_vertex_position;
 out vec4 geom_instance_color;
 
-void EmitLineQuad(vec4 p0, vec4 p1) {
-    vec4 dir = p1 - p0;
+void EmitVertexWithAttributes(int i, vec4 offset) {
+    geom_vertex_normal = vertex_normal[i];
+    geom_vertex_position = vertex_position[i] + offset;
+    geom_instance_color = instance_color[i];
+    gl_Position = gl_in[i].gl_Position + offset;
+    EmitVertex();
+}
+
+void EmitLineQuad(int i0, int i1) {
+    vec4 dir = vertex_position[i1] - vertex_position[i0];
     vec4 offset = normalize(vec4(-dir.y, dir.x, 0.0, 0.0)) * line_width * 0.5;
 
-    geom_vertex_normal = vertex_normal[0];
-    geom_vertex_position = p0 + offset;
-    geom_instance_color = instance_color[0];
-    gl_Position = gl_in[0].gl_Position + offset;
-    EmitVertex();
-
-    geom_vertex_normal = vertex_normal[1];
-    geom_vertex_position = p1 + offset;
-    geom_instance_color = instance_color[1];
-    gl_Position = gl_in[1].gl_Position + offset;
-    EmitVertex();
-
-    geom_vertex_normal = vertex_normal[1];
-    geom_vertex_position = p1 - offset;
-    geom_instance_color = instance_color[1];
-    gl_Position = gl_in[1].gl_Position - offset;
-    EmitVertex();
-
-    geom_vertex_normal = vertex_normal[0];
-    geom_vertex_position = p0 - offset;
-    geom_instance_color = instance_color[0];
-    gl_Position = gl_in[0].gl_Position - offset;
-    EmitVertex();
+    EmitVertexWithAttributes(i0, offset);
+    EmitVertexWithAttributes(i1, offset);
+    EmitVertexWithAttributes(i1, -offset);
+    EmitVertexWithAttributes(i0, -offset);
     
-    // Reuse the first two vertices to form a quad
-    gl_Position = gl_in[0].gl_Position + offset;
-    EmitVertex();
-    
-    gl_Position = gl_in[1].gl_Position + offset;
-    EmitVertex();
-
     EndPrimitive();
 }
 
 void main() {
     if (draw_lines == 0) {
-        // No-op path. just forward attributes along to the fragment shader.
+        // No-op path, just forward attributes along to the fragment shader.
         for (int i = 0; i < 3; ++i) {
-            geom_vertex_normal = vertex_normal[i];
-            geom_vertex_position = vertex_position[i];
-            geom_instance_color = instance_color[i];
-
-            gl_Position = gl_in[i].gl_Position;
-
-            EmitVertex();
+            EmitVertexWithAttributes(i, vec4(0.0));
         }
         EndPrimitive();
     } else {
-        // Draw lines as quads.
-        EmitLineQuad(vertex_position[0], vertex_position[1]);
-        EmitLineQuad(vertex_position[1], vertex_position[2]);
-        EmitLineQuad(vertex_position[2], vertex_position[0]);
+        // Draw triangle lines as quads.
+        EmitLineQuad(0, 1);
+        EmitLineQuad(1, 2);
+        EmitLineQuad(2, 0);
     }
 }
